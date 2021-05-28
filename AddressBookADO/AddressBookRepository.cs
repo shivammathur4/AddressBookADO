ca6fb6de56fe8b7d93461eaa79804b86bc86c112
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-28 10:25:48 +0530

    Add project files.

---
+++ AddressBookADO/AddressBookRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADO
+{
+    public class AddressBookRepository
+    {
+       
+        public static SqlConnection connection { get; set; }
+        
+        public void EnsureDataBaseConnection()
+        {
+            
+            DBConnection dbc = new DBConnection();
+            connection = dbc.GetConnection();
+            using (connection)
+            {
+                Console.WriteLine("The Connection is created");
+            }
+            connection.Close();
+        }
+    }
+}
